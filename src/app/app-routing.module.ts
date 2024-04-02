61ca7013e93ceabab652925ab796d0797fa3dfe1
A.R. Arun Roshan <arunraksha234@gmail.com>
2024-04-02 15:05:04 +0530

    [Arun] Add: Implementing UI for Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Components/login/login.component';
 import { ForgotPasswordComponent } from './Components/forgot-password/forgot-password.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
 
 const routes: Routes = [
   {path: 'login', component:LoginComponent},
-  {path: 'forgot', component:ForgotPasswordComponent}
+  {path: 'forgot', component:ForgotPasswordComponent},
+  {path: 'dashboard', component:DashboardComponent}
 ];
 
 @NgModule({
